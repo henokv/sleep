f7d622a78d433958b0479baa163580eacf326484
Henok Vermeire <me@henokv.dev>
2019-05-13 12:48:08 +0000

    Rewrote some logic

--- src/main/java/dev/henokv/spigot/sleep/Main.java
+++ src/main/java/dev/henokv/spigot/sleep/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new SleepListener(), this);
         super.onEnable();
     }
 }
