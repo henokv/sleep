b2fb2f6c40bae92fe320574db2bd1606b60b0429
Henok Vermeire <me@henokv.dev>
2019-05-23 09:02:50 +0000

    Some info to why time was reset

--- src/main/java/dev/henokv/spigot/sleep/SleepListener.java
+++ src/main/java/dev/henokv/spigot/sleep/SleepListener.java
@@ -13,9 +13,14 @@ public class SleepListener implements Listener {
     public void onPlayerSleep(PlayerBedEnterEvent event){
         if(event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK){
             int inBedCount = 1;
+            double treshold = 0.6;
             Collection<? extends Player> players = Bukkit.getOnlinePlayers();
             for (Player player: players) if(player.isSleeping()) inBedCount += 1;
-            if(inBedCount >= players.size() * 0.6) event.getPlayer().getWorld().setTime(0);
+            double percentage = players.size() * treshold;
+            if(inBedCount >= percentage){
+                event.getPlayer().getWorld().setTime(0);
+                event.getPlayer().getServer().broadcastMessage("Time is set to 0 because "+ percentage +" were in bed" );
+            }
         }
     }
 
